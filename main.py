10b0395beab4c63fed9b972a3347bfcbdc805651
Mack Mason <mackmason16@gmail.com>
2020-08-29 01:20:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+celsius = input("Enter temperature in Celsius: ")
+celsius = float(celsius)
+farenheit = (celsius * (9/5) + 32)
+print(f"{celsius}° in Celsius is equivalent to {farenheit}° Farenheit. ")
