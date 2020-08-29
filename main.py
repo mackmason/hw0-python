fbf4bcb9093d56be4d9f1e648156e116bfbd79b0
Mack Mason <mackmason16@gmail.com>
2020-08-29 01:22:52 +0000

    Changed uppercase letter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-celsius = input("Enter temperature in Celsius: ")
+celsius = input("Enter temperature in celsius: ")
 celsius = float(celsius)
 farenheit = (celsius * (9/5) + 32)
 print(f"{celsius}° in Celsius is equivalent to {farenheit}° Farenheit. ")
