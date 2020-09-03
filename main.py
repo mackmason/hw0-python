80f79c08cfa1f681142327111e61bb6282b5e6a1
Mack Mason <mackmason16@gmail.com>
2020-09-03 13:44:24 +0000

    added #

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Author: Mack Mason mjm8542@psu.edi
+# Collaborator: Brady Bender bmb6369@psu.edu
+# Collaborator: Addy Burchfield abb6060@psu.edu
+# Collaborator: Jacob Hallowell jph5997@psu.edu
+
 temperature = input("Enter temperature: ")
 temperatureChoice = input("Enter unit in F/f or C/c: ")
 temperatureFloat = float(temperature)
